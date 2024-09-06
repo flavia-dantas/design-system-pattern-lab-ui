95e7795daee904631d1c9cea41bf9db43f13e470
flavia-dantas <flavia.dantas2@hotmail.com>
2024-09-05 21:39:49 -0300

    feat: button component creation

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 export * from './components/Avatar'
 export * from './components/Box'
+export * from './components/Button'
 export * from './components/Heading'
 export * from './components/Text'
 
