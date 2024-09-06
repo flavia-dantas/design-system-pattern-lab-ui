4e9fc9f0714b7d535186dcfece6176dc5649d8da
flavia-dantas <flavia.dantas2@hotmail.com>
2024-09-05 21:58:29 -0300

    feat: TextArea component creation

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -4,5 +4,6 @@ export * from './components/Box'
 export * from './components/Button'
 export * from './components/Heading'
 export * from './components/Text'
+export * from './components/TextArea'
 export * from './components/TextInput'
 
