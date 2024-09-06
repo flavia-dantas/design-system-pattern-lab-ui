749eb663193b9f151592cb87b0c0e1666f649e88
flavia-dantas <flavia.dantas2@hotmail.com>
2024-09-05 22:08:44 -0300

    feat: checkbox component creation

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { Check } from 'phosphor-react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+
+export interface CheckboxProps
+  extends React.ComponentPropsWithoutRef<typeof CheckboxContainer> { }
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
