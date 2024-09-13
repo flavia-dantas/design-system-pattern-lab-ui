50c006a5572cd22fd7383b4ad731f00c8ff5033b
flavia-dantas <flavia.dantas2@hotmail.com>
2024-09-12 21:20:19 -0300

    feat: updating the design-system-ignite subdirectory

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,8 +1,13 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/design-system-ignite/',
+  base: '/design-system-ignite/', // Caminho base para o GitHub Pages
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+    },
+  },
+});
